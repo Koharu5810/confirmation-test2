0581ba1f52607db6f04683151ec6ee47e57e61f8
koharu <le2.lsb108@gmail.com>
2024-10-25 19:58:46 +0900

    プロジェクト作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mogitate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <div class="header__logo">
+                <h2>mogitate</h2>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="main-container">
+
+            @yield('content')
+
+        </div>
+    </main>
+</body>
+
+</html>
