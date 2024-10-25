f3b855e8a9efcd25c632fe59231c8f79e7269a24
koharu <le2.lsb108@gmail.com>
2024-10-25 22:21:48 +0900

    seasonsシーダファイル作成

---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $seasons = [
+            "春",
+            "夏",
+            "秋",
+            "冬"
+        ];
+    }
+}
