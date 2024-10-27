c7f81eeb134bc2ef0e783d7e331877500903e69c
koharu <le2.lsb108@gmail.com>
2024-10-28 00:38:55 +0900

    indexビューCSS作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -8,44 +8,45 @@
 @section('content')
 <div class="container">
 {{-- メインコンテンツヘッダー部分 --}}
-    <div class="search-header">
-        <h3 class="search-header__info">商品一覧</h3>
-        <div class="create-button">
-            <a href="{{ route('products.register') }}">+ 商品を追加</a>
-        </div>
+    <div class="product-list__header">
+        <h3 class="product-list__header__info">商品一覧</h3>
+        <a href="{{ route('products.register') }}" class="product-list__add-button">+ 商品を追加</a>
     </div>
-    <div class="layout">
+    <div class="product-list__layout">
 {{-- サイドバー --}}
         <div class="sidebar">
-            <form class="form" action="{{ route('products.search') }}" method="get">
-                <div class="search__form">
+            <form class="sidebar__form" action="{{ route('products.search') }}" method="get">
+            {{-- 検索フォーム --}}
+                <div class="sidebar__search-form">
                     <input type="text" name="search" placeholder="商品名で検索" value="{{ request('search') }}">
                 </div>
                 <button type="submit" class="search__button">検索</button>
-                <label class="sort__title" for="sort">価格順で表示</label>
-                <select name="sort" class="sort-list" onchange="this.form.submit()">
-                    <option value="" disabled {{ request('sort') === null ? 'selected' : '' }}>価格で並べ替え</option>
-                    <option value="high" {{ request('sort') === 'high' ? 'selected' : '' }}>高い順に表示</option>
-                    <option value="low" {{ request('sort') === 'low' ? 'selected' : '' }}>安い順に表示</option>
-                </select>
+            {{-- ソートフォーム --}}
+                <div class="sidebar__sort-form">
+                    <label class="sidebar__sort-title" for="sort">価格順で表示</label>
+                    <select name="sort" class="sidebar__sort-list" onchange="this.form.submit()">
+                        <option value="" disabled {{ request('sort') === null ? 'selected' : '' }}>価格で並べ替え</option>
+                        <option value="high" {{ request('sort') === 'high' ? 'selected' : '' }}>高い順に表示</option>
+                        <option value="low" {{ request('sort') === 'low' ? 'selected' : '' }}>安い順に表示</option>
+                    </select>
         {{-- 並び替えタグ表示 --}}
-            @if (!empty($sortOrder) && ($sortOrder === 'high' || $sortOrder === 'low'))
-                <div class="sort-tag">
-                    <span>{{ $sortOrder === 'high' ? '高い順に表示' : '安い順に表示' }}</span>
-                    <a href="{{ route('products.index', ['search' => request('search')]) }}" class="clear-sort">✕</a>
+                    @if (!empty($sortOrder) && ($sortOrder === 'high' || $sortOrder === 'low'))
+                        <div class="sidebar__sort-tag">
+                            <span>{{ $sortOrder === 'high' ? '高い順に表示' : '安い順に表示' }}</span>
+                            <a href="{{ route('products.index', ['search' => request('search')]) }}" class="sidebar__clear-sort">✕</a>
+                        </div>
+                    @endif
                 </div>
-            @endif
             </form>
         </div>
 {{-- 一覧画面 --}}
         <div class="products">
-            <div class="product-listing">
+            <div class="product-list">
                 @if ($products->count() > 0)
                     @foreach ($products as $product)
                         <a href="{{ route('products.show', ['productId' => $product->id]) }}" class="product-item-link">
-                            <div class="product-item">
                                 <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" />
-                                <div class="product-explanation">
+                                <div class="product-item__explanation">
                                     <div class="product-name">
                                         {{ $product->name }}
                                     </div>
--- src/resources/views/vendor/pagination/semantic-ui.blade.php
+++ src/resources/views/vendor/pagination/semantic-ui.blade.php
@@ -1,26 +1,26 @@
 @if ($paginator->hasPages())
-    <div class="ui pagination menu" role="navigation">
+    <div class="pagination" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+            <a class="pagination__link disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">&lt;</a>
         @else
-            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+            <a class="pagination__link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lt;</a>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <a class="icon item disabled" aria-disabled="true">{{ $element }}</a>
+                <span class="pagination__link disabled" aria-disabled="true">{{ $element }}</span>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
+                        <a class="pagination__link active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
                     @else
-                        <a class="item" href="{{ $url }}">{{ $page }}</a>
+                        <a class="pagination__link" href="{{ $url }}">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
@@ -28,9 +28,9 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
+            <a class="pagination__link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&gt;</a>
         @else
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
+            <a class="pagination__link disabled" aria-disabled="true" aria-label="@lang('pagination.next')">&gt;</a>
         @endif
     </div>
 @endif
